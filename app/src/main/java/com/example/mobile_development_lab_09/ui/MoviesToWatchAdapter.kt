105e204c9d483bd44215861d8b499f4401f2fc61
SomiLast <Lyutinkhay.bk.ru@gmail.com>
2024-12-22 13:11:04 +0700

    Отображение списков фильмов

---
+++ app/src/main/java/com/example/mobile_development_lab_09/ui/MoviesToWatchAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.mobile_development_lab_09.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mobile_development_lab_09.databinding.ItemMovieBinding
+import com.example.mobile_development_lab_09.model.Movie
+import com.example.mobile_development_lab_09.extenstion.fetchImage
+
+class MoviesToWatchAdapter(private val moviesList: List<Movie>) : RecyclerView.Adapter<MoviesToWatchAdapter.MovieViewHolder>() {
+
+    private val selectedMovies = mutableSetOf<Movie>()
+
+    class MovieViewHolder(private val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(movie: Movie, isSelected: Boolean, onSelectChangeListener: (Boolean) -> Unit) {
+            binding.textViewTitle.text = movie.title
+            binding.textViewYear.text = movie.year
+            binding.textViewType.isVisible = false
+            binding.imageViewPoster.fetchImage(movie.poster)
+            binding.checkboxSelect.isChecked = isSelected
+
+            binding.checkboxSelect.setOnCheckedChangeListener { _, isChecked ->
+                onSelectChangeListener(isChecked)
+            }
+
+            itemView.setOnClickListener {
+                binding.checkboxSelect.isChecked = !binding.checkboxSelect.isChecked
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(moviesList[position], selectedMovies.contains(moviesList[position])) { isSelected ->
+            if (isSelected) {
+                selectedMovies.add(moviesList[position])
+            } else {
+                selectedMovies.remove(moviesList[position])
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = moviesList.size
+
+    fun getSelectedMovies(): List<Movie> {
+        return selectedMovies.toList()
+    }
+}
